7e0c4e7b91f887af2af3adbd61e9e97b947b0e23
ayomideRussianCode <ayindeayomide7@gmail.com>
2025-11-09 04:12:42 +0100

    login page created

---
+++ .php_tmp/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+   <h2>Login Page</h2> 
+
+   <form method="POST">
+
+   <label for="username">Username</label>
+   <input id="username" type = "text" name="username"><br> <br>
+     <label for="password">Password</label>
+   <input id="password" type = "text" name="password"> <br><br>
+   <input type="submit" value="Login">
+
+   </form>
+</body>
+</html>
